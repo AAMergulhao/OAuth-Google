222b4742465c5549fdaf75a769405a0eba5ebc18
Arthur <luckas.ribeiro2015@gmail.com>
2019-05-03 12:48:03 -0300

    Mongo

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("../config/keys")
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("users")
 
 passport.use(
     new GoogleStrategy(
@@ -9,7 +12,7 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: "/auth/google/callback"
     }, (accessToken, refreshToken, profile, done) => {
-        new User({googleID: profile_id}.save())
+        new User({googleID: profile.id}).save()
     }
    )
 );
