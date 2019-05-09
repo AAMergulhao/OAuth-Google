f496ac6827fe94067ca01091094a09a36ea9c307
AAMergulhão <alves.m.arthur@gmail.com>
2019-05-09 18:01:53 -0300

    OAuth Header login updates

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,37 @@
 import React, {Component} from "react";
+import {connect} from "react-redux";
 
-class Header extends Component {
+class Header extends Component{
+  renderContent(){
+    switch (this.props.auth){
+      case null:
+          return;
+      case false:
+          return <li><a href="/auth/google">Login With Google</a></li>
+      case true:
+        return <li><a href="">Logout</a></li>
+      default:
+        return <li><a href="/auth/google">a</a></li>
+        
+    }
+  }
+
     render (){
+        console.log(this.props);
         return(
-            <div>
-                Header
+            <nav>
+            <div className="nav-wrapper green">
+              <a href="#" className="left brand-logo">Nexus</a>
+              <ul id="nav-mobile" className="right">
+                {this.renderContent()}
+              </ul>
             </div>
+          </nav>
         )
     }
 }
 
-export default Header;
+function mapStateToProps({auth}){
+  return{auth};
+}
+export default connect(mapStateToProps)(Header);
