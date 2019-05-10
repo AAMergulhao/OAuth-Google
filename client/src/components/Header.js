1d26729d8bfd346ea62bdf83b336f5ca71fd1aa9
AAMergulhão <alves.m.arthur@gmail.com>
2019-05-10 16:08:30 -0300

    Payment function

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,27 +1,29 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
+import {Link} from "react-router-dom";
+import Payments from "./Payments";
 
 class Header extends Component{
   renderContent(){
     switch (this.props.auth){
       case null:
-          return;
+          return "Resolvin...";
       case false:
           return <li><a href="/auth/google">Login With Google</a></li>
-      case true:
-        return <li><a href="">Logout</a></li>
       default:
-        return <li><a href="/auth/google">a</a></li>
+        return [
+                <li key="1"><Payments /></li>,
+                <li key="2"><a href="/api/logout">Logout</a></li>
+               ];
         
     }
   }
 
     render (){
-        console.log(this.props);
         return(
             <nav>
             <div className="nav-wrapper green">
-              <a href="#" className="left brand-logo">Nexus</a>
+              <Link to={this.props.auth ? "/surveys" : "/"} href="#" className="left brand-logo">Nexus</Link>
               <ul id="nav-mobile" className="right">
                 {this.renderContent()}
               </ul>
---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Landing = () =>{
+    return(
+        <div style={{textAlign: "center"}}>
+            <h1>
+                Nexus
+            </h1>
+            Fala meu querido
+        </div>
+    );
+};
+
+export default Landing;
