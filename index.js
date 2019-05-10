a8762e0f122c3582d41a0d62f0acb41ef24543ca
AAMergulhão <alves.m.arthur@gmail.com>
2019-05-10 18:26:34 -0300

    .

--- index.js
+++ index.js
@@ -25,6 +25,11 @@ require("./routes/billingRoutes")(app);
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
-
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"))
+    app.get("*", (req,res) =>{
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
